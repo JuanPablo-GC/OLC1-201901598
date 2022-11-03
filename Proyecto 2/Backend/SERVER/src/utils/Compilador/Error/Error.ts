90c04fe576308abd2d39058110c964fe7920cea0
Juan Pablo García <3730058490309@ingenieria.usac.edu.gt>
2022-11-03 17:36:53 -0600

    Error

--- Proyecto 2/Backend/SERVER/src/utils/Compilador/Error/Error.ts
+++
@@ -1,8 +0,0 @@
-export class error {
-    constructor(
-        public tipo: string,
-        public descripcion: string,
-        public linea: number,
-        public columna: number
-    ) { }
-}
